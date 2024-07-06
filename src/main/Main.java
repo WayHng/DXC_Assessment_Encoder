da4f46a962d84181c3e6cd616d29a1bf0b3b19b6
Hoong <wayhng@gmail.com>
2024-07-06 18:50:24 +0800

    Modified Main to prompt for and accept terminal inputs.

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,10 @@
 package main;
 
 import test.*;
+import service.Encoder;
+import service.Decoder;
+
+import java.util.Scanner;
 
 public class Main 
 {
@@ -20,6 +24,53 @@ public class Main
         EncoderTest testfile3 = new EncoderTest();
         testfile3.test();
         // */
+
+        Scanner input = new Scanner(System.in);
+        boolean state = true;
+        Encoder encoder = new Encoder();
+        Decoder decoder = new Decoder();
+        String plainText;
+        String encodedText;
+
+        while(state) {
+            try {
+                System.out.printf("Select option:\n1. Encode\n2. Decode\n3. Exit\n");
+                int num = input.nextInt();
+                input.nextLine(); //To consume next line feed that is ingnored in "input.nextInt()".
+
+                switch(num) {
+                    case 1:
+                        System.out.printf("Enter Plain Text: ");
+                        plainText = input.nextLine();
+                        encodedText = encoder.encode(plainText);
+                        System.out.printf("The encoded text of \"%s\": %s\n\n", plainText, 
+                                encodedText);
+                        break;
+                    case 2:
+                        System.out.printf("Enter Encoded Text: ");
+                        encodedText = input.nextLine();
+                        plainText = decoder.decode(encodedText);
+
+                        if(plainText != null) {
+                            System.out.printf("The plain text of \"%s\": %s\n\n", 
+                                    encodedText, plainText);
+                        }
+                        else {
+                            System.out.printf("Error: Encoded Text is invalid.\n\n");
+                        }
+                        break;
+                    case 3:
+                        state = false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            catch (Exception e) {
+                input.nextLine(); //To consume next line feed that is ingnored in "input.nextInt()".
+                System.out.printf("Error has occurred. Kindly restart from beginning.\n\n");
+            }
+        }
 
         System.out.printf("End of Program.\n");
     }
